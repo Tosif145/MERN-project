8028e1eec9a1cf96d1bd16aa7b8998f8dff4c51e
Tosif145 <tklearning2000@gmail.com>
2024-07-30 10:22:21 +0530

    backend/ all category controller functions

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,7 +1,9 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import Category from "../models/categoryModel.js";
+import mongoose from "mongoose";
 
 
+//  Create a new category`
 const createCategory = asyncHandler( async (req, res) => {
     try {
         const {name } = req.body;
@@ -25,6 +27,111 @@ const createCategory = asyncHandler( async (req, res) => {
         console.log(error);
         return res.status(400).json(error)
     }
-})
+});
 
-export {createCategory}
+
+// Update existing category by id
+const updateCategory = asyncHandler( async (req, res)  => {
+    try {
+        const {categoryId} = req.params;
+        if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+          }
+        const category = await Category.findById(categoryId);
+        const {name} = req.body;
+
+        if(!category){
+            return res.status(404).json({error: 'Category not found!'});
+        }
+
+        category.name = name || category.name;
+        const newCategory = await category.save();
+
+        return res.status(200).json({ 
+            id: newCategory._id,
+            name: newCategory.name,
+            message: 'Category updated successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error);
+    }
+});
+
+
+// Get all categories
+const getAllCategories = asyncHandler(async (req, res) => {
+    try {
+        const categories = await Category.find({});
+
+        return res.status(200).json({
+            categories,
+            message: 'Categories fetched successfully'
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error);
+    }
+});
+
+
+// Delete category by id
+const deleteCategory = asyncHandler(async (req, res) => {
+    try {
+
+        const {categoryId} = req.params;
+        if (!mongoose.Types.ObjectId.isValid(categoryId)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+        
+        const removed = await Category.findByIdAndDelete(categoryId);
+        if(!removed){
+            return res.status(404).json({error: 'Category not found!'});
+        }
+        return res.status(200).json({
+            message: 'Category removed successfully'
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error);
+    }
+});
+
+// Get category by id
+const readCategory = asyncHandler(async (req, res) => {
+    try {
+        const {id} = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
+        const category = await Category.findById({_id: id});
+
+        if (!category) {
+            return res.status(404).json({error: 'Category not found!'});
+        }
+
+        return res.status(200).json({
+            _id: category._id,
+            name: category.name,
+            message: 'Category fetched successfully'
+        })
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json(error);
+    }
+});
+
+
+export {
+         createCategory,
+         updateCategory,
+         getAllCategories,
+         readCategory,
+         deleteCategory
+      } 
+
+
+    
--- backend/routes/CategoryRoutes.js
+++ backend/routes/CategoryRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 import { authenticateUser ,authorizeAdmin} from "../middlewares/authMiddleware.js";
-import { createCategory } from "../controllers/categoryController.js";
+import { createCategory, deleteCategory, getAllCategories, readCategory, updateCategory } from "../controllers/categoryController.js";
  
 
 /**
@@ -59,5 +59,193 @@ import { createCategory } from "../controllers/categoryController.js";
  *                   example: Category already exist
  */
 router.post('/',authenticateUser, authorizeAdmin, createCategory);
+
+
+/**
+ * @openapi
+ * /api/category/{categoryId}:
+ *   put:
+ *     summary: Update category (Admin only)
+ *     description: Update existing category
+ *     tags:
+ *       - Category
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Name of the category
+ *     parameters:
+ *       - name: categoryId
+ *         in: path
+ *         required: true
+ *         description: ID of the category to update
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Category updated successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: string
+ *                 name:
+ *                   type: string
+ *                   description: name of the category
+ *       400:
+ *         description: Name is required or Invalid Id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message: 
+ *                   type: string
+ *                   example: Name is required
+ *       404:
+ *         description: Category not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Category not found
+ */
+router.put('/:categoryId',authenticateUser, authorizeAdmin, updateCategory);
+
+
+
+/**
+ * @openapi
+ * /api/category/categories:
+ *   get:
+ *     summary: Retrieve all categoriers (Admin only)
+ *     description: Fetches a list of all categoriers. Requires authentication and admin authorization.
+ *     tags:
+ *       - Category
+ *     security:
+ *       - jwtBearerAuth: []  # Indicates that Bearer token authentication is required
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved all categoriers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   _id:
+ *                     type: string
+ *                     description: The unique identifier for the category
+ *                   name:
+ *                     type: string
+ *                     description: The name of the user
+ *       401:
+ *         description: Not authorized, Login first
+ *       403:
+ *         description: Not authorized as an admin
+ */
+
+router.get('/categories',authenticateUser, authorizeAdmin, getAllCategories);
+
+/**
+ * @openapi
+ * /api/category/{id}:
+ *   get:
+ *     summary: Retrieve  category (Admin only)
+ *     description: Fetch the category. Requires authentication and admin authorization.
+ *     tags:
+ *       - Category
+ *     security:
+ *       - jwtBearerAuth: []  # Indicates that Bearer token authentication is required
+  *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: ID of the category to get
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved the category
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: Object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   description: The unique identifier for the category
+ *                 name:
+ *                   type: string
+ *                   description: The name of the category
+ *       404:
+ *         description: Category not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Category not found
+ *       401:
+ *         description: Not authorized, Login first
+ *       403:
+ *         description: Not authorized as an admin
+ */
+router.get('/:id',authenticateUser, authorizeAdmin, readCategory);
+
+
+/**
+ * @openapi
+ * /api/category/{categoryId}:
+ *   delete:
+ *     summary: Delete Category (Admin only)
+ *     description: Delete the category if the user is admin.
+ *     tags:
+ *       - Category
+ *     parameters:
+ *       - name: categoryId
+ *         in: path
+ *         required: true
+ *         description: ID of the category to delete
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Category deleted successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Category deleted successfully
+ *       404:
+ *         description: Category not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: Category not found
+ */
+router.delete('/:categoryId',authenticateUser, authorizeAdmin, deleteCategory);
 
 export default router;
